aaefae7f67b677be3ddcc6a1fb63a4b42e63c591
Tuomas Hietanen <tuomas.hietanen@basware.com>
2015-12-11 04:41:26 +0200

    Update generated documentation for version 1.0.1

---
+++ ExampleProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Owin;
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(MyServer.MyWebStartup))]
+
+namespace MyServer
+{
+    
+    class MyWebStartup
+    {
+        public void Configuration(Owin.IAppBuilder app)
+        {
+            var settings = OwinCompression.DefaultCompressionSettingsWithPath(@"c:\temp\");
+            //or var settings = new CompressionSettings( ... )
+            app.MapCompressionModule("/zipped", settings);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Owin.Hosting.WebApp.Start<MyWebStartup>("http://*:8080");
+            Console.WriteLine("Server started... Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
