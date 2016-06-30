762998ab3435343582f6caaf3f6abb1d3e2fa550
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-06-30 13:54:20 +0100

    Bump version to 1.0.10

--- src/Owin.Compression/AssemblyInfo.fs
+++ src/Owin.Compression/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Owin.Compression")>]
 [<assembly: AssemblyProductAttribute("Owin.Compression")>]
 [<assembly: AssemblyDescriptionAttribute("Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline.")>]
-[<assembly: AssemblyVersionAttribute("1.0.9")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.9")>]
+[<assembly: AssemblyVersionAttribute("1.0.10")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.10")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.9"
-    let [<Literal>] InformationalVersion = "1.0.9"
+    let [<Literal>] Version = "1.0.10"
+    let [<Literal>] InformationalVersion = "1.0.10"
