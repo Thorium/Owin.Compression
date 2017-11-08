53d91654f02c21dfa39f7e46e1932586fb5e8a40
Tuomas Hietanen <thorium@iki.fi>
2017-11-08 18:29:42 +0000

    Bump version to 1.0.18

--- src/Owin.Compression.Standard/AssemblyInfo.fs
+++ src/Owin.Compression.Standard/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Owin.Compression.Standard")>]
 [<assembly: AssemblyProductAttribute("Owin.Compression")>]
 [<assembly: AssemblyDescriptionAttribute("Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline.")>]
-[<assembly: AssemblyVersionAttribute("1.0.17")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.17")>]
+[<assembly: AssemblyVersionAttribute("1.0.18")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.18")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Owin.Compression.Standard"
     let [<Literal>] AssemblyProduct = "Owin.Compression"
     let [<Literal>] AssemblyDescription = "Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline."
-    let [<Literal>] AssemblyVersion = "1.0.17"
-    let [<Literal>] AssemblyFileVersion = "1.0.17"
+    let [<Literal>] AssemblyVersion = "1.0.18"
+    let [<Literal>] AssemblyFileVersion = "1.0.18"
