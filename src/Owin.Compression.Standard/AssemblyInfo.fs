01d4bbf13f9979079dc8f2881d46aec02890e6df
Tuomas Hietanen <thorium@iki.fi>
2021-02-01 11:53:50 +0000

    FSharp.Core reference updated, 1.0.22

--- src/Owin.Compression.Standard/AssemblyInfo.fs
+++ src/Owin.Compression.Standard/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Owin.Compression.Standard")>]
 [<assembly: AssemblyProductAttribute("Owin.Compression")>]
 [<assembly: AssemblyDescriptionAttribute("Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline.")>]
-[<assembly: AssemblyVersionAttribute("1.0.21")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.21")>]
+[<assembly: AssemblyVersionAttribute("1.0.22")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.22")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Owin.Compression.Standard"
     let [<Literal>] AssemblyProduct = "Owin.Compression"
     let [<Literal>] AssemblyDescription = "Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline."
-    let [<Literal>] AssemblyVersion = "1.0.21"
-    let [<Literal>] AssemblyFileVersion = "1.0.21"
+    let [<Literal>] AssemblyVersion = "1.0.22"
+    let [<Literal>] AssemblyFileVersion = "1.0.22"
