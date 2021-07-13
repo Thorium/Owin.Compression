749f72dd78d7611482ba15e00360881e59f4dbe0
Tuomas Hietanen <thorium@iki.fi>
2021-07-13 14:34:14 +0100

    component updates

--- src/Owin.Compression/AssemblyInfo.fs
+++ src/Owin.Compression/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Owin.Compression")>]
 [<assembly: AssemblyProductAttribute("Owin.Compression")>]
 [<assembly: AssemblyDescriptionAttribute("Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline.")>]
-[<assembly: AssemblyVersionAttribute("1.0.22")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.22")>]
+[<assembly: AssemblyVersionAttribute("1.0.23")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.23")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Owin.Compression"
     let [<Literal>] AssemblyProduct = "Owin.Compression"
     let [<Literal>] AssemblyDescription = "Compression (Deflate / GZip) module for Microsoft OWIN Selfhost filesystem pipeline."
-    let [<Literal>] AssemblyVersion = "1.0.22"
-    let [<Literal>] AssemblyFileVersion = "1.0.22"
+    let [<Literal>] AssemblyVersion = "1.0.23"
+    let [<Literal>] AssemblyFileVersion = "1.0.23"
